93ee0df41880bb366a751453cc0acf5347c0f99d
dev <aldhigandia1@gmail.com>
2024-03-03 22:32:47 +0800

    prepare docker compose

--- scripts/docker-compose/run.sh
+++ scripts/docker-compose/run.sh
@@ -6,4 +6,4 @@ set +a
 # Use the `envsubst` command to substitute the shell environment variables into reference_var.env and output to a combined .env
 find ./ -type f \( -iname "*.env" -o -iname "docker-compose.yaml" \) ! -name "common.env" -exec /bin/bash -c 'file="{}";cp "$file" "$file.bak"; envsubst < "$file.bak" > "$file"; rm "$file.bak"' \;
 
-sudo -E docker-compose up -d
+docker-compose up -d
